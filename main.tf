8f41e06f939d518a2cbafe6fe008cca353451842
vaisshnavi1703 <potlapallivaisshnavi@gmail.com>
2025-05-23 14:01:03 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -2,44 +2,86 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 5.0"
     }
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
       version = "~> 3.0"
     }
   }
-  required_version = ">= 1.5.0"
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-east-1"
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
-# Use default VPC
-data "aws_vpc" "default" {
-  default = true
+variable "AWS_ACCESS_KEY_ID" {
+  type        = string
+  description = "Your AWS access key"
 }
 
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-}
-
-data "aws_key_pair" "example_key" {
-  key_name = "example-key"
+variable "AWS_SECRET_ACCESS_KEY" {
+  type        = string
+  description = "Your AWS secret key"
 }
 
 resource "random_id" "sg_suffix" {
   byte_length = 4
 }
 
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "main-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "main-route-table"
+  }
+}
+
+resource "aws_route_table_association" "main" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.main.id
+}
+
 resource "aws_security_group" "de" {
   name        = "de-${random_id.sg_suffix.hex}"
-  description = "Security group for EC2"
-  vpc_id      = data.aws_vpc.default.id
+  description = "Allow SSH and HTTP"
+  vpc_id      = aws_vpc.main.id
 
   ingress {
     from_port   = 22
@@ -63,16 +105,17 @@ resource "aws_security_group" "de" {
   }
 
   tags = {
-    Name = "de"
+    Name = "de-${random_id.sg_suffix.hex}"
   }
 }
 
-module "example_ec2" {
-  source             = "./modules/ec2"
-  ami                = "ami-084568db4383264d4"
-  instance_type      = "t2.micro"
-  subnet_id          = element(data.aws_subnets.default.ids, 0)
-  security_group_ids = [aws_security_group.de.id]
-  key_name           = data.aws_key_pair.example_key.key_name
-  instance_name      = "example-ec21-instance"
+resource "aws_instance" "web" {
+  ami                    = "ami-0c02fb55956c7d316"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.main.id
+  vpc_security_group_ids = [aws_security_group.de.id]
+
+  tags = {
+    Name = "web-instance"
+  }
 }
