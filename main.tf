8ac0c033794966bb2b93722d6dff2346ad7b907c
vaisshnavi1703 <potlapallivaisshnavi@gmail.com>
2025-05-23 14:08:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -61,7 +61,7 @@ resource "aws_security_group" "de" {
 
 module "example_ec2" {
   source             = "./modules/ec2"
-  ami                = "ami-084568db4383264d4"
+  ami                = "ami-0953476d60561c955"
   instance_type      = "t2.micro"
   subnet_id          = element(data.aws_subnets.default.ids, 0)
   security_group_ids = [aws_security_group.de.id]
